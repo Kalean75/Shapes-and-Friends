e47f3b06eda5ada03afbce8fcf12766a61c607a2
Amanda Duong <107091242+xiumew@users.noreply.github.com>
2022-06-16 12:46:26 -0600

    ScrollingBackground
    Updated Scroller Code & created Parallax Scrolling image and implied it to Cosmo Background test image.

--- Shapes And Friends/Assets/Scripts/Scroller.cs
+++ Shapes And Friends/Assets/Scripts/Scroller.cs
@@ -16,5 +16,11 @@ public class Scroller : MonoBehaviour
     void Update()
     {
         //Add renderer to background
+        transform.position += new Vector3(-5 * Time.deltaTime, 0);
+
+        if (transform.position.x < -21.5)
+        {
+            transform.position = new Vector3(21.5f, transform.position.y);
+        }
     }
 }
