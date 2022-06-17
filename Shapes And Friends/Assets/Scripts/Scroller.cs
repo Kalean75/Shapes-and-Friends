b6bfb8ddaab26832d183798d8cf04108a069764f
Kalean75 <whitedevint@gmail.com>
2022-06-17 02:27:53 -0600

    Various additions for testing purposes

--- Shapes And Friends/Assets/Scripts/Scroller.cs
+++ Shapes And Friends/Assets/Scripts/Scroller.cs
@@ -4,6 +4,9 @@ public class Scroller : MonoBehaviour
 {
 	//serializeField makes the value editable within unity if clicking on object
 	[SerializeField] float scrollSpeed = 5f;
+
+	//testing purposes remove later
+	[SerializeField] bool scroll = true;
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -13,12 +16,15 @@ public class Scroller : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		//Add renderer to background
-		transform.position += new Vector3(-scrollSpeed * Time.deltaTime, 0);
-
-		if (transform.position.x < -10)
+		if (scroll)
 		{
-			transform.position = new Vector3(10f, transform.position.y);
+			//Add renderer to background
+			transform.position += new Vector3(-scrollSpeed * Time.deltaTime, 0);
+
+			if (transform.position.x < -25)
+			{
+				transform.position = new Vector3(25f, transform.position.y);
+			}
 		}
 	}
 }
