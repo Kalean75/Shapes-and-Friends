8b752ceecbb8f1d9c18069d270a0a12754018ff8
robinblack2002 <39348959+robinblack2002@users.noreply.github.com>
2022-07-11 20:07:54 -0600

    UI Start and Pause Menu
    Basic UIs without too much art assets

---
+++ Shapes And Friends/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isActive = false;
+    // Update is called once per frame
+    public GameObject PauseUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //Application.Quit();
+            if (!isActive)
+            {
+                Pause();
+                
+            }
+            else
+            {
+                Resume();
+                
+            }
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        PauseUI.SetActive(true);
+        isActive = true;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        PauseUI.SetActive(false);
+        isActive = false;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
