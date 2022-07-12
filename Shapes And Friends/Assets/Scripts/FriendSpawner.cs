95699145f5fd23d3825808ed5a5c8909b25137aa
Devin White <WhiteDevint@Gmail.com>
2022-07-11 22:31:09 -0600

    Changed spawner to not need asset dragged in,

--- Shapes And Friends/Assets/Scripts/FriendSpawner.cs
+++ Shapes And Friends/Assets/Scripts/FriendSpawner.cs
@@ -9,7 +9,7 @@ public class FriendSpawner : MonoBehaviour
     float spawnTimer;
     //[SerializeField] int MaxSpawnedFriends = 1;
     [SerializeField] GameObject friend;
-    [SerializeField] GameObject spawner;
+    //[SerializeField] GameObject spawner;
     private int currentlySpawnedFriends = 0;
     // Start is called before the first frame update
     void Start()
@@ -24,7 +24,7 @@ public class FriendSpawner : MonoBehaviour
         //Debug.Log(spawnTimer);
         if (spawnTimer <= 0f)
         {
-            Instantiate(friend, new Vector3(spawner.transform.position.x, spawner.transform.position.y, spawner.transform.position.z), Quaternion.identity);
+            Instantiate(friend, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), Quaternion.identity);
             incrementCurrentlySpawnedFriends();
             spawnTimer = Random.Range(minSpawnRate, maxSpawnRate);
             Debug.Log(currentlySpawnedFriends);
