c693532054b77e2f18102fd2e145731a5ef0476a
robinblack2002 <robinblack2002@gmail.com>
2022-08-02 05:53:59 -0600

    UI Changes & Stage Of Life Tracker

--- Shapes And Friends/Assets/Scripts/StageOfLife.cs
+++ Shapes And Friends/Assets/Scripts/StageOfLife.cs
@@ -2,23 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class StageOfLife : MonoBehaviour
 {
 	public TextMeshProUGUI textBox;
 	public new GameObject collider;
+	private bool moving;
+	private bool next;
+	void Start()
+    {
+		moving = false;
+		next = false;
+	}
+	void Update()
+    {
+        if (moving)
+        {
+			transform.position += new Vector3(0.06f * Time.deltaTime, 0);
+			GetComponent<SpriteRenderer>().color = Color.Lerp(GetComponent<SpriteRenderer>().color, Color.white, Time.deltaTime * 1);
+			if (next)
+			{
+				GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().changeStageOfLife();//Change player stage of life to the next stage
+				next = false;
+			}
+		}
 
-
+	}
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		string[] Stage = { "Toddler", "Adolecense", "Young Adult", "Adult", "Elder" }; //List of which stages of life is load from. 
+		string[] Stage = { "Childhood", "Adolecense", "Young Adulthood", "Adulthood", "Elder","Death" }; //List of which stages of life is load from.
+		
 		//string[] Stage = {"Apprentice", "Cultist", "Prophet", "Saint" };
-		if (collision.CompareTag("Player"))
-		{
+		if (collision.CompareTag("UI")) { 
+			collider.SetActive(false);//Disable collider prefab
 			int i = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().getStageOfLife();//Get current stage of life from player
 			textBox.text = Stage[i];
-			GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().changeStageOfLife();//Change player stage of life to the next stage
-			collider.SetActive(false);//Disable collider prefab
+			
+			if (i > 4)
+            {
+				SceneManager.LoadScene(2);
+            }
+			moving = true;
+			Debug.Log(i.ToString());
+			next = true;
 		}
 	}
 
