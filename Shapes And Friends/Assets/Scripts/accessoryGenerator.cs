3d611d341dcdc13791c4cb87c1af8a27f3cd3eac
Kalean75 <43306850+Kalean75@users.noreply.github.com>
2023-08-12 00:12:39 -0600

    Suddenly, changes appeared to improve original intent of the game.
    Tweaks to make it more in line with the vision, such as having the growth determine what accessories will spawn

--- Shapes And Friends/Assets/Scripts/accessoryGenerator.cs
+++ Shapes And Friends/Assets/Scripts/accessoryGenerator.cs
@@ -7,17 +7,20 @@ public class accessoryGenerator : MonoBehaviour
     [Header("Possible Accessories")]
     [SerializeField] List<Sprite> accessories = new List<Sprite>();
     SpriteRenderer accessory;
+    int stageOfLife;
     // Start is called before the first frame update
     void Start()
     {
         accessory = GetComponent<SpriteRenderer>();
         generateAccesory(Random.Range(0, accessories.Count + 1));
+        stageOfLife = GetComponentInParent<Player>().getStageOfLife();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        int index = Random.Range(stageOfLife, accessories.Count);
+        generateAccesory(index);
     }
     private void generateAccesory(int index)
     {
